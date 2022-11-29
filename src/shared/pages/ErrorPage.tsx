f61a489012da2bbe408f258aee10c59a1089f805
raudzius <raudziusmykolas@gmail.com>
2022-11-29 07:47:45 +0200

    feat: error page

---
+++ src/shared/pages/ErrorPage.tsx
@@ -0,0 +1,33 @@
+import { useRouteError } from 'react-router-dom';
+import {
+ Alert, AlertTitle, Box, Typography,
+} from '@mui/material';
+import React from 'react';
+
+type ErrorResponse = {
+  data: null;
+  status: 404;
+  statusText: 'Not Found';
+};
+
+const ErrorPage = () => {
+  const error: ErrorResponse = useRouteError() as ErrorResponse;
+  console.error(error);
+
+  return (
+    <Box sx={{
+ width: '30%', height: '100vh', mx: 'auto', mt: '40vh',
+}}
+    >
+      <Alert severity="error">
+        <AlertTitle>Error</AlertTitle>
+        <Typography component="h2" variant="h5">
+          Sorry, an unexpected error has occurred.
+        </Typography>
+        <Typography>{error.statusText}</Typography>
+      </Alert>
+    </Box>
+  );
+};
+
+export default ErrorPage;
