7aa0c9d8215a1227703c7625f116112c176ba916
raudzius <raudziusmykolas@gmail.com>
2022-11-29 04:14:16 +0200

    feat: initial page & route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import * as React from 'react';
 import { Box } from '@mui/material';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AllProducts from './shared/pages/AllProducts';
+import NewProduct from './admin/pages/NewProduct';
 
-const App = () => <Box>My app</Box>;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AllProducts />,
+  },
+  {
+    path: 'signup',
+    element: <div>Signup</div>,
+  },
+  {
+    path: 'signin',
+    element: <div>Signup</div>,
+  },
+  {
+    path: 'products/new',
+    element: <NewProduct />,
+  },
+]);
+
+const App: React.FC = () => (
+  <Box>
+    <RouterProvider router={router} />
+  </Box>
+);
 
 export default App;
---
+++ src/admin/pages/NewProduct.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NewProduct = () => <div>NewProduct</div>;
+
+export default NewProduct;
