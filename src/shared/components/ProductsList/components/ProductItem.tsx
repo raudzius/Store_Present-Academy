29d51fb4e60cadcd7d9c27e19741b91b31d6c03b
raudzius <raudziusmykolas@gmail.com>
2022-11-29 12:05:29 +0200

    feat: product card redirects on click

--- src/shared/components/ProductsList/components/ProductItem.tsx
+++ src/shared/components/ProductsList/components/ProductItem.tsx
@@ -1,7 +1,8 @@
 import {
- ListItem, Card, CardMedia, CardContent, Typography,
+ ListItem, Card, CardMedia, CardContent, Typography, Link,
 } from '@mui/material';
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { type Product } from '../../../pages/AllProductsPage';
 
 type ProductItemProps = Product;
@@ -9,18 +10,20 @@ type ProductItemProps = Product;
 const ProductItem: React.FC<ProductItemProps> = ({ id, title, image }) => (
   <ListItem>
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia component="img" alt="product" height="200" image={image} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-          {id}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem atque, dolore earum
-          dolorem ipsa tempore voluptatibus quasi reiciendis aut doloribus eos, culpa sint debitis
-          quisquam mollitia ratione velit ut. Ad!
-        </Typography>
-      </CardContent>
+      <Link component={RouterLink} to={`/products/${id}`} color="inherit" underline="none">
+        <CardMedia component="img" alt="product" height="200" image={image} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+            {id}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem atque, dolore earum
+            dolorem ipsa tempore voluptatibus quasi reiciendis aut doloribus eos, culpa sint debitis
+            quisquam mollitia ratione velit ut. Ad!
+          </Typography>
+        </CardContent>
+      </Link>
     </Card>
   </ListItem>
 );
