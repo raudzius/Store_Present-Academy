af1b159b7c6861e1c54698abf002638e40dbdd8b
raudzius <raudziusmykolas@gmail.com>
2022-11-29 11:06:28 +0200

    feat: create header

---
+++ src/shared/components/Navigation/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+ AppBar, Toolbar, IconButton, Button,
+} from '@mui/material';
+import { Menu } from '@mui/icons-material';
+import { Link as RouterLink } from 'react-router-dom';
+
+const Header = () => (
+  <AppBar position="sticky">
+    <Toolbar>
+      <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <Menu />
+      </IconButton>
+      <Button component={RouterLink} to="products/all" color="inherit">
+        All
+      </Button>
+      <Button component={RouterLink} to="products/new" color="inherit">
+        New
+      </Button>
+    </Toolbar>
+  </AppBar>
+);
+
+export default Header;
