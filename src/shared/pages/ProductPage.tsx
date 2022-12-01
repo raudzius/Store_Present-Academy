34d5ba5ee9af96e8d3e64992945670c47988ac86
raudzius <raudziusmykolas@gmail.com>
2022-12-01 11:05:06 +0200

    feat: use data passed down through a link

--- src/shared/pages/ProductPage.tsx
+++ src/shared/pages/ProductPage.tsx
@@ -7,11 +7,11 @@ import { useLocation } from 'react-router-dom';
 const ProductPage: React.FC = () => {
   const {
  id, title, image, description,
-} = useLocation().state.data;
+} = useLocation().state;
 
   return (
-    <Card sx={{ height: 400 }}>
-      <CardMedia component="img" alt="product" height="200" image={image} />
+    <Card sx={{ height: 600 }}>
+      <CardMedia component="img" alt="product" height="400" image={image} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
