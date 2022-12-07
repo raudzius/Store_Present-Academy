d97b235c32127b4a3fe034ff5266d095fc8f3f83
raudzius <raudziusmykolas@gmail.com>
2022-12-07 10:04:32 +0200

    feat: init customer orders page

--- src/authorized/customer/pages/CustomerOrdersPage.tsx
+++ src/authorized/customer/pages/CustomerOrdersPage.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import OrdersList from '../../shared/components/OrdersList';
 
-const CustomerOrdersPage = () => <div>CustomerOrdersPage</div>;
+const orders: Product[] = [
+  {
+    id: 'p1',
+    title: 'Shrimp - 150 - 250',
+    image: 'http://dummyimage.com/194x100.png/5fa2dd/ffffff',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem atque, dolore earumdolorem ipsa tempore voluptatibus quasi reiciendis aut doloribus eos, culpa sint debitis quisquam mollitia ratione velit ut. Ad!',
+  },
+  {
+    id: 'p2',
+    title: 'Beef Dry Aged Tenderloin Aaa',
+    image: 'http://dummyimage.com/147x100.png/ff4444/ffffff',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem atque, dolore earumdolorem ipsa tempore voluptatibus quasi reiciendis aut doloribus eos, culpa sint debitis quisquam mollitia ratione velit ut. Ad!',
+  },
+  {
+    id: 'p3',
+    title: 'Ice - Clear, 300 Lb For Carving',
+    image: 'http://dummyimage.com/175x100.png/ff4444/ffffff',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem atque, dolore earumdolorem ipsa tempore voluptatibus quasi reiciendis aut doloribus eos, culpa sint debitis quisquam mollitia ratione velit ut. Ad!',
+  },
+];
+
+const CustomerOrdersPage: React.FC = () => <OrdersList orders={orders} />;
 
 export default CustomerOrdersPage;
